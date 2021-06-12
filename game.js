a9be0f5ceb1e46d3288511cf9022a5cbe3559999
jborza <jborza@gmail.com>
2021-06-12 13:12:56 +0200

    upgrade to vuejs, scaling more stuff

--- game.js
+++ game.js
@@ -1,29 +1,22 @@
 let operandCount = 2;
-let coins = 0;
-let diamonds = 0;
-let maxA = 5;
-let maxB = 5;
-let machines_1 = 0;
-let difficultyBase = 10;
 
 function result_changed() {
     let resultBox = document.getElementById("result");
     let result = resultBox.value;
-    let operands = [];
-    for (let i = 0; i < operandCount; i++) {
-        let id = "term-" + (i + 1);
-        operands.push(Number(document.getElementById(id).value));
-    }
-    let expectedResult = operands[0] + operands[1];
+    // let operands = [];
+    // for (let i = 0; i < operandCount; i++) {
+    //     let id = "term-" + (i + 1);
+    //     operands.push(ope);
+    // }
+    let expectedResult = app.operands[0] + app.operands[1];
     if (expectedResult == result) {
         resultBox.style.opacity = 0;
-        resultBox.style.transition = "opacity 0.25s";
+        resultBox.style.transition = "opacity 0.35s";
 
         //make a new problem
         makeNewProblem();
         //add coins
-        coins += Math.ceil(getDifficulty(operands[0]) + getDifficulty(operands[1]));
-        showCoins();
+        app.coins += getReward();
     }
     else {
         resultBox.style.opacity = 1;
@@ -38,67 +31,71 @@ Math.logBase = (function() {
   })();
 
 function getDifficulty(n){
-    return Math.logBase(n, difficultyBase);
+    return Math.logBase(n, app.difficulty);
 }
 
 function lower_difficulty(){
-    if(diamonds < 20)
+    if(app.diamonds < app.difficultyUpgradeCost)
         return;
-    difficultyBase = difficultyBase * 0.95;
-    document.getElementById("difficulty").textContent = difficultyBase;
+    app.difficultyUpgradeCost = incrementWithScale(app.difficultyUpgradeCost,  1.06);
+    app.difficulty = app.difficulty * 0.95;
+    app.reward = getReward();
 }
 
 function rand(max) {
     return Math.ceil(Math.random() * max);
 }
 
+function getReward(){
+    return Math.ceil(getDifficulty(app.operands[0]) + getDifficulty(app.operands[1]));
+}
+
 function makeNewProblem() {
-    document.getElementById("term-1").value = rand(maxA);
-    document.getElementById("term-2").value = rand(maxB);
-}
-
-function showCoins() {
-    document.getElementById("coins").textContent = coins;
-}
-
-function showDiamonds() {
-    document.getElementById("diamonds").textContent = diamonds;
+    app.operands[0] = rand(app.maxA);
+    app.operands[1] = rand(app.maxA);
+    app.reward = getReward();
 }
 
 function buy_machine_1() {
     //check for cost
     //TODO scale machine cost
-    if (coins < 10)
+    if (app.coins < app.machineCost[0])
         return;
-    coins -= 10;
-    showCoins();
-    machines_1++;
-    document.getElementById("machine-1-count").textContent = machines_1;
+    
+    app.coins -= app.machineCost[0];
+    app.machineCost[0] = incrementWithScale(app.machineCost[0], 1.09);
+    app.machinesOwned[0]++;
 }
 
 function increment_base(n){
     //increment by some sane base, maybe 1.1 and we'll see
-    let growthFactor = 1.1; //TODO upgradable
-    let newN = n * 1.1;
+    const growthFactor = 1.1; //TODO upgradable
+    let newN = n * growthFactor;
     return Math.ceil(newN);
 }
 
 function increment_number(n) {
     //TODO scale the cost faster than the number increments, maybe 1.15
-    if (diamonds < 20)
-        return;
     if (n == 1) {
+        if(app.diamonds < app.numbersUpgradeCost[0])
+            return;
         //increment by some sane base, maybe 1.1
-        maxA = increment_base(maxA);
-        diamonds -= 20;
-        document.getElementById("number-1-max").textContent = maxA;
+        app.maxA = incrementWithScale(app.maxA, 1.1);
+        app.diamonds -= app.numbersUpgradeCost[0];
+        app.numbersUpgradeCost[0] = incrementWithScale(app.numbersUpgradeCost[0], 1.15);
     }
     else if (n == 2) {
+        if(app.diamonds < app.numbersUpgradeCost[1])
+            return;
         //increment by some sane base
-        maxB = increment_base(maxB);
-        diamonds -= 20;
-        document.getElementById("number-2-max").textContent = maxB;
+        app.maxB = incrementWithScale(app.maxB, 1.1);
+        app.diamonds -= app.numbersUpgradeCost[1];
+        app.numbersUpgradeCost[1] = incrementWithScale(app.numbersUpgradeCost[1], 1.15);
     }
+}
+
+function incrementWithScale(n, scale){
+    return Math.ceil(n*scale);
 }
 
 function result_transitionend() {
@@ -110,12 +107,11 @@ function result_transitionend() {
 
 function machineTick() {
     //TODO tick more often, use floating point for diamonds, but show entire ones
-    diamonds += machines_1;
-    showDiamonds();
+    app.diamonds += app.machinesOwned[0];
 }
 
 function init() {
-    coins = 0;
+    app.coins = 0;
     makeNewProblem();
 }
 
@@ -126,3 +122,20 @@ setTimeout(function () {
 setInterval(() => {
     machineTick();
 }, 1000);
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        coins: 0,
+        diamonds: 0,
+        difficultyUpgradeCost: 20,
+        numbersUpgradeCost: [10, 10],
+        machinesOwned: [0,0,0],
+        difficulty: 10,
+        machineCost: [10,0,0],
+        maxA: 5,
+        maxB: 5,
+        operands: [0,0],
+        reward: 0
+    }
+});
